563c41067d9e0c1b6d2a177ce31f2c6cbc953f9b
Vikalp Saini <42584833+Vikalp03@users.noreply.github.com>
2019-04-06 01:59:52 +0530

    Login form by using Python Flask Framework

---
+++ hello.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+
+
+#certain parameters need to be configured to connect this app to the database
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'userdata'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+
+	        #fetch the user details
+		admin_details = request.form
+		email = admin_details['Email']
+		password = admin_details['Password']
+		cur = mysql.get_db().cursor()
+		cur.execute("INSERT INTO credentials(username,password) VALUES(%s, %s)",(email, password))
+		mysql.connection.commit()
+
+		return 'Success'
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
